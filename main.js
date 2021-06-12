1d962c76b2c314534a029c47945fa990413b0bc2
DworshiFH <71599996+DworshiFH@users.noreply.github.com>
2021-06-12 19:10:26 +0200

    main.html prepared fo JS

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    class Event{
+        constructor(eventTitle, eventDesc) {
+            this.eventTitle = eventTitle;
+            this.eventDesc = eventDesc;
+        }
+    }
+
+    class Dashboard {
+        constructor() {
+            this.mainEventScreen = document.getElementById("mainEventScreen");
+        }
+
+        addEventToScreen(event){
+
+        }
+    }
+
+
+
+});
