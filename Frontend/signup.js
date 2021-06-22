664e53f9950e9898b7733c8d6bcbc868f06e7ded
Richard Cox <71561900+RichardCox09@users.noreply.github.com>
2021-06-22 16:27:24 +0200

    signup AAAAAALMOST working

--- Frontend/signup.js
+++ Frontend/signup.js
@@ -1,4 +1,4 @@
-const signup = () =>{
+/*const signup = () =>{
     const postURL = 'http://localhost:3000/user/register';
     const form = document.getElementById(form);
     console.log(form);
@@ -24,4 +24,39 @@ const signup = () =>{
 }
 
 const button = document.getElementById('signUp');
-button.addEventListener('click', signup);
+button.addEventListener('click', signup);*/
+
+const form = document.getElementById('reg-form');
+form.addEventListener('submit', registerUser);
+
+async function registerUser(event){
+    event.preventDefault();
+
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('passwordConfirm').value;
+
+    const toSend = JSON.stringify({
+        firstname: firstName,
+        lastname: lastName,
+        email: email,
+        password: password,
+        password: passwordConfirm
+    });
+
+    console.log(toSend);
+
+    const result = await fetch('/user/register', {
+        method: 'POST',
+        header: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: toSend
+    })//.then((res) => res.json());
+
+    console.log(result);
+
+}
