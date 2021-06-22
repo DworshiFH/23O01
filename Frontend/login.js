1dd0e01b5743b63bb12a5ee3087dcc5421771600
Patrick Schlosser <71377227+patrickschl@users.noreply.github.com>
2021-06-22 15:58:40 +0200

    added login.js (commented out)

---
+++ Frontend/login.js
@@ -0,0 +1,21 @@
+/*const loginForm = document.getElementById("form");
+const loginButton = document.getElementById("form-submit");
+this.loginButton = document.createElement("Button");
+this.Button.appendChild("loginButton");
+
+
+
+
+loginButton.addEventListener("click", (e) =>{
+    e.preventDefault()();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "web_dev"){
+        alert("You have successfully logged in.");
+        window.location = "eventPage.html";
+    } else{
+        alert("Login failed. Try again with right credentials you dumbass >:-(");
+        location.reload();
+    }
+})*/
